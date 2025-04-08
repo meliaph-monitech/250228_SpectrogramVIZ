d3320a923dcae79aa3eb1bde96bfbabfc8afc945
meliaph-monitech <meliaph.monitech@gmail.com>
2025-04-08 14:38:21 +0900

    Update 250226_work_spectrogramviz.py

--- 250226_work_spectrogramviz.py
+++ 250226_work_spectrogramviz.py
@@ -60,6 +60,14 @@ with st.sidebar:
                 st.success("Bead segmentation complete")
                 st.session_state["metadata"] = metadata
 
+fs = st.sidebar.number_input("Sampling Frequency (fs)", min_value=1000, max_value=50000, value=10000)
+a = st.sidebar.number_input("nperseg parameter (a)", min_value=256, max_value=4096, value=1024)
+b = st.sidebar.number_input("Division Factor (b)", min_value=1, max_value=10, value=4)
+c = st.sidebar.number_input("Overlap Ratio (c)", min_value=0.0, max_value=1.0, value=0.99)
+d = st.sidebar.number_input("nfft parameter (d)", min_value=512, max_value=8192, value=2048)
+db_scale = st.sidebar.number_input("dB Scale", min_value=50, max_value=150, value=110)
+ylimit = st.sidebar.number_input("Y-Axis Limit", min_value=100, max_value=int(fs / 2), value=500)
+
 if "metadata" in st.session_state and isinstance(st.session_state["metadata"], dict):
     selected_file = st.sidebar.selectbox("Select a CSV file", list(st.session_state["metadata"].keys()))
     
@@ -70,13 +78,20 @@ if "metadata" in st.session_state and isinstance(st.session_state["metadata"], d
         column_options = df.columns[:2].tolist()
         selected_column = st.sidebar.radio("Select Data Column", column_options)
         
+        if "selected_frequencies" not in st.session_state:
+            st.session_state["selected_frequencies"] = []
+        
+        frequency = st.sidebar.number_input("Enter Frequency (Hz)", min_value=1, value=240)
+        if st.sidebar.button("Add Frequency"):
+            if frequency not in st.session_state["selected_frequencies"]:
+                st.session_state["selected_frequencies"].append(frequency)
+        
         start, end = st.session_state["metadata"][selected_file][selected_bead - 1]
         sample_data = df.iloc[start:end, :2].values
-        fs = 10000
-        nperseg = min(1024, len(sample_data) // 4)
-        noverlap = int(0.99 * nperseg)
-        nfft = min(2048, 4 ** int(np.ceil(np.log2(nperseg * 2))))
-        db_scale = 110
+        
+        nperseg = min(a, len(sample_data) // b)
+        noverlap = int(c * nperseg)
+        nfft = min(d, b ** int(np.ceil(np.log2(nperseg * 2))))
         
         f, t, Sxx = signal.spectrogram(sample_data[:, df.columns.get_loc(selected_column)], fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
         Sxx_dB = 20 * np.log10(np.abs(Sxx) + np.finfo(float).eps)
@@ -85,8 +100,24 @@ if "metadata" in st.session_state and isinstance(st.session_state["metadata"], d
         
         fig, ax = plt.subplots(figsize=(6, 4))
         img = ax.pcolormesh(t, f, Sxx_dB - min_disp_dB, shading='gouraud', cmap='jet')
-        ax.set_ylim([0, 500])
+        ax.set_ylim([0, ylimit])
         ax.set_ylabel("Frequency (Hz)")
         ax.set_xlabel("Time (s)")
         fig.colorbar(img, ax=ax, aspect=20)
         st.pyplot(fig)
+        
+        if st.session_state["selected_frequencies"]:
+            fig, ax = plt.subplots(figsize=(6, 4))
+            for freq in st.session_state["selected_frequencies"]:
+                freq_indices = np.where((f >= freq - 5) & (f <= freq + 5))[0]
+                if len(freq_indices) > 0:
+                    intensity_over_time = np.mean(Sxx_dB[freq_indices, :] - min_disp_dB, axis=0)
+                    ax.plot(t, intensity_over_time, label=f"{freq} Hz")
+            
+            ax.set_xlabel("Time (s)")
+            ax.set_ylabel("Signal Intensities (dB)")
+            ax.legend()
+            st.pyplot(fig)
+        
+        if st.sidebar.button("Clear Frequencies"):
+            st.session_state["selected_frequencies"] = []
